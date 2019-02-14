e7abff406244209b609ce79971232572ba875d87
Rafael Afonso <rafaelafonso1980@gmail.com>
2019-02-14 10:32:21 +0000

    Framework project initialization

---
+++ Communicator/Communicator.h
@@ -0,0 +1,19 @@
+//
+//  Communicator.h
+//  Communicator
+//
+//  Created by Rafael Afonso on 14/02/2019.
+//  Copyright © 2019 Rafael Afonso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Communicator.
+FOUNDATION_EXPORT double CommunicatorVersionNumber;
+
+//! Project version string for Communicator.
+FOUNDATION_EXPORT const unsigned char CommunicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Communicator/PublicHeader.h>
+
+
